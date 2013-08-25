fa4ed1012f9667d94945aa0b221b775ecb67c10c
folivi folly <ab.folivi@gmail.com>
2013-08-25 05:00:14 +0200

    dd

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -6,8 +6,11 @@ class Artist
   field :name, type: String
   field :lastname, type: String
   field :country, type: String
+  field :rank, type: String
   
   mount_uploader :picture, PictureUploader
   
   slug :name, history: true
+  
+  default_scope order_by(:rank.asc)
 end
